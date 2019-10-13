b11dfb47ae85fb90718a93691a4b7dee87125716
n1kko777 <3734489@gmail.com>
2019-10-13 11:48:00 +0500

    Изменение в модели

--- backend/src/operations/models.py
+++ backend/src/operations/models.py
@@ -9,12 +9,6 @@ from django.db import models
 class Operation(models.Model):
     credit = models.DecimalField(max_digits=10, decimal_places=2)
     removeFromAmount = models.BooleanField(default=True)
-    title = models.CharField(max_length=120)
-    comment = models.TextField()
+    category = models.CharField(max_length=120)
+    wallet = models.CharField(max_length=120)
     created_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-
-    def __unicode__(self):
-        return u"{}".format(self.title)
