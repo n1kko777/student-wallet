267cc85a0ea0596f0e343657c07ee8be14579b29
n1kko777 <3734489@gmail.com>
2019-11-15 23:00:11 +0500

    Стилизация меню

--- frontend/gui/src/components/layouts/MainMenu.js
+++ frontend/gui/src/components/layouts/MainMenu.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Menu, Dropdown, Icon } from "antd";
 
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { connect } from "react-redux";
 import { logout } from "../../store/actions/auth";
@@ -9,62 +9,57 @@ import { logout } from "../../store/actions/auth";
 import PropTypes from "prop-types";
 
 const MainMenu = ({ isAuth, onLogout }) => {
-  const { pathname } = useLocation();
-
   const authUser = (
-    <Menu>
-      <Menu.Item style={{ fontSize: "14px" }}>
-        <Link rel='noopener noreferrer' to='/settings'>
-          <Icon type='setting' /> Настройки
+    <Menu className="main-menu">
+      <Menu.Item>
+        <Link rel="noopener noreferrer" to="/settings">
+          <Icon type="setting" /> Настройки
         </Link>
       </Menu.Item>
-      <Menu.Item style={{ fontSize: "14px" }}>
-        <Link rel='noopener noreferrer' to='/reports'>
-          <Icon type='form' /> Отчет
+      <Menu.Item>
+        <Link rel="noopener noreferrer" to="/reports">
+          <Icon type="form" /> Отчет
         </Link>
       </Menu.Item>
-      <Menu.Item style={{ fontSize: "14px" }} onClick={onLogout}>
-        <Icon type='logout' /> Выход
+      <Menu.Item onClick={onLogout}>
+        <Link rel="noopener noreferrer" to="/">
+          <Icon type="logout" /> Выход
+        </Link>
       </Menu.Item>
     </Menu>
   );
 
   const guestUser = (
     <Menu
-      mode='horizontal'
-      theme='dark'
+      mode="horizontal"
+      theme="dark"
       style={{ lineHeight: "64px" }}
       selectable={false}
     >
       <Menu.Item>
         <Link
           style={{ fontSize: "14px" }}
-          rel='noopener noreferrer'
-          to='/login'
+          rel="noopener noreferrer"
+          to="/login"
         >
-          <Icon type='login' /> Войти
+          <Icon type="login" /> Войти
         </Link>
       </Menu.Item>
       <Menu.Item>
         <Link
           style={{ fontSize: "14px" }}
-          rel='noopener noreferrer'
-          to='/about'
+          rel="noopener noreferrer"
+          to="/about"
         >
-          <Icon type='info' /> О приложении
+          <Icon type="info" /> О приложении
         </Link>
       </Menu.Item>
     </Menu>
   );
 
-  if (
-    isAuth &&
-    (pathname === "/feed" ||
-      pathname === "/settings" ||
-      pathname === "/reports")
-  ) {
+  if (isAuth) {
     return (
-      <Dropdown overlay={authUser} placement='bottomRight' trigger={["click"]}>
+      <Dropdown overlay={authUser} placement="bottomRight" trigger={["click"]}>
         <Icon
           type={"setting"}
           style={{ cursor: "pointer", color: "#fff", fontSize: "24px" }}
@@ -93,7 +88,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(MainMenu);
+export default connect(mapStateToProps, mapDispatchToProps)(MainMenu);
